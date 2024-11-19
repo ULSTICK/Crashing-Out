74ea56ea7302e1c93f886bea1121e1298796b7bc
MarkScedra <marcoscedraz@live.com>
2024-11-18 23:51:52 -0300

    Coming Back Update
    Adding Sync and some mods to make it compatible with Forge

--- kubejs/startup_scripts/custom_fluids/pyrotheum.js
+++ kubejs/startup_scripts/custom_fluids/pyrotheum.js
@@ -2,8 +2,29 @@ StartupEvents.registry('fluid', event => {event.create('pyrotheum')
 .displayName('Pyrotheum')
 .stillTexture('kubejs:block/fluid_pyrotheum_still')
 .flowingTexture('kubejs:block/fluid_pyrotheum_flow')
+.thickTexture(0xffdc25)
 .bucketColor(0xffdc25)
 .luminosity(15)
-.temperature(3000)
-.viscosity(3000)
+.temperature(3000000)
+.viscosity(1000000)
+.rarity("rare")
+.density(2000)
 })
+
+// Startup script
+/* onEvent('fluid.registry', event => {
+    // These first examples are 1.16.5 and 1.18.2 syntax
+    
+    // Basic "thick" (looks like lava) fluid with red tint
+    event.create('pyrotheum')
+      .thickTexture(0xffdc25)
+      .bucketColor(0xffdc25)
+      .displayName('Pyrotheum')
+      .luminosity(15)
+      .temperature(3000)
+      .viscosity(1200)
+      .rarity("rare")
+      .density(2000)
+                 
+
+  }) */
