f39eca484889284e90818e64566870652916ac56
MeiNanita <mei-nanita@gmx.net>
2025-01-12 02:23:31 +0100

    removed redundant code at removed_recipes.js and removed exdeorum sieve-defaults

--- kubejs/server_scripts/remove/removed_recipes.js
+++ kubejs/server_scripts/remove/removed_recipes.js
@@ -1,11 +1,7 @@
 ServerEvents.recipes(event => {
-    event.remove({ output: 'cold_sweat:filled_waterskin' })
-})
+    event.remove({ output: 'cold_sweat:filled_waterskin' });
+    event.remove({ output: 'cold_sweat:waterskin' });
+    event.remove({ output: 'minecraft:leather', type: 'yaam:drying_recipe' });
 
-ServerEvents.recipes(event => {
-    event.remove({ output: 'cold_sweat:waterskin' })
-})
-
-ServerEvents.recipes(event => {
-    event.remove({ output: 'minecraft:leather', type: 'yaam:drying_recipe' })
-})
+    exdeorum.removeDefaultSieveRecipes(event);
+});
