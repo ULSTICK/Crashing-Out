ce817e2623aa05b4a88ef01a1cb3823d0501ba23
MarkScedra <marcoscedraz@live.com>
2025-01-16 03:49:24 -0300

    disabled high tier canteens, created shapeless crafting to fill the leather canteen

---
+++ kubejs/server_scripts/water_shapeless/acceptable/canteen_increment_water_2_acceptable.js
@@ -0,0 +1,175 @@
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:0,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:1,Purity:2}')
+)
+
+})
+
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:0,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:2,Purity:2}')
+)
+
+})
+
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:1,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:3,Purity:2}')
+)
+
+})
+
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:2,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:4,Purity:2}')
+)
+
+})
+
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:3,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:5,Purity:2}')
+)
+
+})
+
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:4,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:6,Purity:2}')
+)
+
+})
+
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:5,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:7,Purity:2}')
+)
+
+})
+
+ServerEvents.recipes(event => {
+  function filling(output, waterInput, canteenInput) {
+event.shapeless(output.strongNBT(), [
+
+      canteenInput.strongNBT(),
+      waterInput.strongNBT(),
+      waterInput.strongNBT()
+
+    ]).replaceIngredient('minecraft:potion', 'minecraft:glass_bottle')
+}  
+
+filling(
+  // output
+  Item.of('thirstcanteen:leather_canteen_full', '{Damage:6,Purity:2}'),
+  // waterInput
+  Item.of('minecraft:potion', '{Potion:"minecraft:water",Purity:2}'),
+  // canteenInput
+  Item.of('thirstcanteen:leather_canteen')
+)
+
+})
