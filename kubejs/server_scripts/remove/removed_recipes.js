4f9d74c8c5a8c2d31995d738fc0586d540c06dd0
MarkScedra <marcoscedraz@live.com>
2025-01-18 21:26:46 -0300

    failed trying to change the output water bottle on the bottler

--- kubejs/server_scripts/remove/removed_recipes.js
+++ kubejs/server_scripts/remove/removed_recipes.js
@@ -2,6 +2,7 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'cold_sweat:filled_waterskin' });
     event.remove({ output: 'cold_sweat:waterskin' });
     event.remove({ output: 'minecraft:leather', type: 'yaam:drying_recipe' });
+   /// event.remove({ output: Item.of('minecraft:potion', '{Potion:"minecraft:water"}'), type:'thermal:bottler' });
     event.remove({ output: 'thirstcanteen:military_bottle' });
     event.remove({ output: 'thirstcanteen:dragon_bottle' });
     event.remove({ output: 'exdeorum:end_cake' });
