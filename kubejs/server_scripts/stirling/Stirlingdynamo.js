3b90b155f92b39aec7e53005ce4d4791a37dbb4d
NagasakiiD <hryan520000@gmail.com>
2025-01-26 21:09:41 -0500

    changed stirling dynamo generator recipe
    Changed the recipe so it can be used as a starter generator as it uses fuel such as coal, charcoal, and wood

---
+++ kubejs/server_scripts/stirling/Stirlingdynamo.js
@@ -0,0 +1,17 @@
+ServerEvents.recipes(event => {
+    // Remove the original recipe for the Stirling Dynamo
+    event.remove({ output: 'thermal:dynamo_stirling' });
+
+    // Add the new custom recipe for the Stirling Dynamo
+    event.shaped('thermal:dynamo_stirling', [
+        ' R ',
+        'IGI',
+        'SBS'
+    ], {
+        R: 'thermal:rf_coil',
+        I: 'minecraft:iron_ingot',
+        G: 'thermal:iron_gear',
+        S: '#forge:stone',
+        B: 'minecraft:redstone_block'
+    });
+});
