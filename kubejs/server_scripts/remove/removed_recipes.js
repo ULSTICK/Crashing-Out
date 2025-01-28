3e3db530c4e042bb9ed3cdd8afe7a42b9557c54e
MarkScedra <marcoscedraz@live.com>
2025-01-28 16:54:36 -0300

    chest loot config/removed magma generator from industrial foregoing

--- kubejs/server_scripts/remove/removed_recipes.js
+++ kubejs/server_scripts/remove/removed_recipes.js
@@ -2,6 +2,7 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'minecraft:leather', type: 'yaam:drying_recipe' });
     event.remove({ output: 'exdeorum:end_cake' });
     event.remove({ output: 'thermal:dynamo_magmatic' });
+    event.remove({ output: 'industrialforegoing:mycelial_magma' });
     event.remove({ type: 'exdeorum:compressed_sieve' });
     event.remove({ mod: 'simplyjetpacks' });
     event.remove({ id: 'tconstruct:molten_pewter' })
