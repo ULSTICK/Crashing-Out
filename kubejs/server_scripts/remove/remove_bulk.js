bb151e318d371ffdd5958dc31e283254d3d8497f
MarkScedra <marcoscedraz@live.com>
2025-02-02 04:04:17 -0300

    some adjustments

---
+++ kubejs/server_scripts/remove/remove_bulk.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(event => {
+    event.remove({ output: 
+        [
+            "exdeorum:warped_crucible",
+            "exdeorum:crimson_crucible",
+            "exdeorum:oak_crucible",
+            "exdeorum:spruce_crucible",
+            "exdeorum:birch_crucible",
+            "exdeorum:jungle_crucible",
+            "exdeorum:acacia_crucible",
+            "exdeorum:dark_oak_crucible",
+            "exdeorum:bamboo_crucible"
+        ]
+     });    
+});
--- kubejs/server_scripts/remove/removed_recipes.js
+++ kubejs/server_scripts/remove/removed_recipes.js
@@ -6,7 +6,11 @@ ServerEvents.recipes(event => {
     event.remove({ type: 'exdeorum:compressed_sieve' });
     event.remove({ mod: 'simplyjetpacks' });
     event.remove({ mod: 'ad_astra' });
-    event.remove({ id: 'tconstruct:molten_pewter' })
+    event.remove({ id: 'tconstruct:molten_pewter' });
+    event.remove({ output: 'minecraft:potion', type: 'tconstruct:casting_table' });
+    event.remove({ type: 'tconstruct:casting_table_potion' });
+    event.remove({ type: 'exdeorum:water_crucible' });
+    
 
     exdeorum.removeDefaultSieveRecipes(event);
 });
