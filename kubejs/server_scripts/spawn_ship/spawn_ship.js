555296986ee752f2de66d7ae5f75f7837bc41124
MarkScedra <marcoscedraz@live.com>
2025-02-09 21:27:41 -0300

    fixed the lasts changes before 1.3.6 part 2

--- kubejs/server_scripts/spawn_ship/spawn_ship.js
+++ kubejs/server_scripts/spawn_ship/spawn_ship.js
@@ -2,5 +2,4 @@ ServerEvents.loaded(event =>{
     if (event.server.persistentData.firstLoad) return
     event.server.persistentData.firstLoad = true
     event.server.runCommandSilent('execute run place structure crash_landing:ship ~-5 ~5 ~')
-   && event.server.runCommandSilent('execute run give @p hardcorequesting:quest_book')
 })
