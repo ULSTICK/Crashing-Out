4d841e40d24ce7b3f8d1bc509be7503b3b03457f
MarkScedra <marcoscedraz@live.com>
2025-02-15 22:05:04 -0300

    updated the ship spawn to avoid spawning inside the building

--- kubejs/server_scripts/spawn_ship/spawn_ship.js
+++ kubejs/server_scripts/spawn_ship/spawn_ship.js
@@ -1,5 +1,5 @@
 ServerEvents.loaded(event =>{
     if (event.server.persistentData.firstLoad) return
     event.server.persistentData.firstLoad = true
-    event.server.runCommandSilent('execute run place structure crash_landing:ship ~-5 ~5 ~')
+    event.server.runCommandSilent('execute run place structure crash_landing:ship ~-5 ~ ~')
 })
---
+++ kubejs/server_scripts/tags/shears.js
@@ -0,0 +1,7 @@
+ServerEvents.tags('item', event => {
+    // Get the #forge:cobblestone tag collection and add Diamond Ore to it
+    event.add('forge:shears', 'extra_food_items:invarshears')
+    event.add('c:shears', 'extra_food_items:invarshears')
+    event.add('bookshelf:shears', 'extra_food_items:invarshears')
+    event.add('forge:tools/invar', 'extra_food_items:invarshears')
+  })
