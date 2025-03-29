c9709b42db90600f34ed723462a8ff742ef72e7a
Jeremy <pawjwp@users.noreply.github.com>
2025-03-29 13:34:01 -0400

    Replaced Gravestone with Corpse + small mod tweaks
    Removed duplicate Bucketlib mod file
    Replace Gravestone with Corpse
    Added Stackables for higher item stacks for some items (most important for easier potion crafting)
    Increased stack size of salty water to match Stackables changes
    Added corresponding config files

--- kubejs/startup_scripts/salty/salty_water_bottle.js
+++ kubejs/startup_scripts/salty/salty_water_bottle.js
@@ -2,25 +2,23 @@ StartupEvents.registry('item', event => {
 
 
     event.create('salty_water_bottle')
-    .maxStackSize(1)
-    .displayName('Salty Water Bottle')
-    .useAnimation('drink')
-    .useDuration(itemstack => 64)
-    .use((level, player, hand) => true)
-    .finishUsing((itemstack, level, entity) => {
+        .maxStackSize(16)
+        .displayName('Salty Water Bottle')
+        .useAnimation('drink')
+        .useDuration(itemstack => 64)
+        .use((level, player, hand) => true)
+        .finishUsing((itemstack, level, entity) => {
 
-        itemstack.shrink(1)
+            itemstack.shrink(1)
 
-        entity.give('minecraft:glass_bottle')
+            entity.give('minecraft:glass_bottle')
 
-        let effects = entity.potionEffects;
-        effects.add("poison", 200, 10, false, true)
-        effects.add("survive:thirst", 600, 0, false, true)
+            let effects = entity.potionEffects;
+            effects.add("poison", 200, 10, false, true)
+            effects.add("survive:thirst", 600, 0, false, true)
 
-     
-    }
-         
-)
-return
-  })
 
+        }
+        )
+    return
+})
