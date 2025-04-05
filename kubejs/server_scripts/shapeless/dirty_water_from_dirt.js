74f8a99fffdef2bac010811773e14410b99429df
Jeremy <pawjwp@users.noreply.github.com>
2025-04-05 19:09:29 -0400

    Recipe tweaks
    Increased output from melting plastic scrap
    Increased difficulty of Mekanism wind generator
    Slightly increased difficulty of Mekanism solar generator
    Added second dirty water recipe with a water bucket

--- kubejs/server_scripts/shapeless/dirty_water_from_dirt.js
+++ kubejs/server_scripts/shapeless/dirty_water_from_dirt.js
@@ -2,7 +2,16 @@ ServerEvents.recipes(event => {
     event.shapeless(
         Item.of('minecraft:potion', '{Potion:"minecraft:water"}'), [
             'minecraft:glass_bottle',
-           '#minecraft:dirt'
+            '#minecraft:dirt'
+        ]
+    )
+})
+
+ServerEvents.recipes(event => {
+    event.shapeless(
+        Item.of('survive:purified_water_bucket'), [
+            'minecraft:water_bucket',
+            '#minecraft:dirt'
         ]
     )
 })
--- kubejs/server_scripts/shapeless/planks_to_slabs.js
+++ kubejs/server_scripts/shapeless/planks_to_slabs.js
@@ -1,26 +1,26 @@
 ServerEvents.recipes(event => {
-  function slabed(output, input) {
+  function slabbed(output, input) {
     event.shapeless(output, [
       input,
     ])
   }
 
-  slabed('minecraft:oak_planks', '2x minecraft:oak_slab'),
-  slabed('minecraft:spruce_planks', '2x minecraft:spruce_slab'),
-  slabed('minecraft:birch_planks', '2x minecraft:birch_slab'),
-  slabed('minecraft:jungle_planks', '2x minecraft:jungle_slab'),
-  slabed('minecraft:acacia_planks', '2x minecraft:acacia_slab'),
-  slabed('minecraft:dark_oak_planks', '2x minecraft:dark_oak_slab'),
-  slabed('minecraft:mangrove_planks', '2x minecraft:mangrove_slab'),
-  slabed('minecraft:cherry_planks', '2x minecraft:cherry_slab'),
-  slabed('minecraft:bamboo_planks', '2x minecraft:bamboo_slab'),
-  slabed('minecraft:crimson_planks', '2x minecraft:crimson_slab'),
-  slabed('minecraft:warped_planks', '2x minecraft:warped_slab'),
-  slabed('thermal:rubberwood_planks', '2x thermal:rubberwood_slab'),
-  slabed('tconstruct:greenheart_planks', '2x tconstruct:greenheart_planks_slab'),
-  slabed('tconstruct:skyroot_planks', '2x tconstruct:skyroot_planks_slab'),
-  slabed('tconstruct:bloodshroom_planks', '2x tconstruct:bloodshroom_planks_slab'),
-  slabed('tconstruct:enderbark_planks', '2x tconstruct:enderbark_planks_slab')
-  slabed('minecraft:cobblestone', '2x minecraft:cobblestone_slab')
-  slabed('minecraft:stone', '2x minecraft:stone_slab')
+  slabbed('minecraft:oak_planks', '2x minecraft:oak_slab'),
+  slabbed('minecraft:spruce_planks', '2x minecraft:spruce_slab'),
+  slabbed('minecraft:birch_planks', '2x minecraft:birch_slab'),
+  slabbed('minecraft:jungle_planks', '2x minecraft:jungle_slab'),
+  slabbed('minecraft:acacia_planks', '2x minecraft:acacia_slab'),
+  slabbed('minecraft:dark_oak_planks', '2x minecraft:dark_oak_slab'),
+  slabbed('minecraft:mangrove_planks', '2x minecraft:mangrove_slab'),
+  slabbed('minecraft:cherry_planks', '2x minecraft:cherry_slab'),
+  slabbed('minecraft:bamboo_planks', '2x minecraft:bamboo_slab'),
+  slabbed('minecraft:crimson_planks', '2x minecraft:crimson_slab'),
+  slabbed('minecraft:warped_planks', '2x minecraft:warped_slab'),
+  slabbed('thermal:rubberwood_planks', '2x thermal:rubberwood_slab'),
+  slabbed('tconstruct:greenheart_planks', '2x tconstruct:greenheart_planks_slab'),
+  slabbed('tconstruct:skyroot_planks', '2x tconstruct:skyroot_planks_slab'),
+  slabbed('tconstruct:bloodshroom_planks', '2x tconstruct:bloodshroom_planks_slab'),
+  slabbed('tconstruct:enderbark_planks', '2x tconstruct:enderbark_planks_slab')
+  slabbed('minecraft:cobblestone', '2x minecraft:cobblestone_slab')
+  slabbed('minecraft:stone', '2x minecraft:stone_slab')
 })
