1d1402239c0d033a71a9b55f5d11863048d53d1a
Jeremy <pawjwp@users.noreply.github.com>
2025-04-26 23:55:54 -0400

    Assorted recipe changes
    Allowed daylight detectors to be crafted with certus quartz
    Allowed writable books to be crafted with any black dyes
    Adjusted crumbling seared bricks tag and adjusted recipe for future-proofing
    Removed changes to rice crop loot table so the block drops seeds again
    Slightly boosted infested leaves drops
    
    Added Ex Deorum dust and crushed stone recipes to Create crushing wheel, Ender IO SAG mill, Mekanism Crusher, and Thermal pulverizer:
    - Blackstone
    - Deepslate
    - End Stone
    - Netherrack
    
    Sawdust Changes:
    - Added wood to the material unification list to merge sawdusts together
    - Added wood dust tags to Ex Deorum wood chippings
    - Added Create milling and crushing recipe to crush logs into sawdust
    - Added Ender IO SAG mill recipe to crush logs into sawdust
    - Adjusted Thermal pulverizer sawdust recipe to be consistent with other recipes

---
+++ kubejs/server_scripts/tags/wood_chippings.js
@@ -0,0 +1,6 @@
+ServerEvents.tags('item', event => {
+  event.add('c:dusts', 'exdeorum:wood_chippings')
+  event.add('forge:dusts', 'exdeorum:wood_chippings')
+  event.add('forge:dusts/wood', 'exdeorum:wood_chippings')
+  event.add('forge:sawdust', 'exdeorum:wood_chippings')
+})
