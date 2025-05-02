042cc0d9adcd6b745f17c657f1e0215c680d5dd5
Jeremy <pawjwp@users.noreply.github.com>
2025-05-02 11:42:33 -0400

    Dynamo recipe standardization + recipe tweak
    Standardized dynamo recipes (Magmatic still disabled)
    Removed old YAAM recipes
    Reduced plastic chilling energy cost

--- kubejs/server_scripts/recipes/remove/removed_recipes.js
+++ kubejs/server_scripts/recipes/remove/removed_recipes.js
@@ -1,7 +1,5 @@
 ServerEvents.recipes(event => {
-    event.remove({ output: 'minecraft:leather', type: 'yaam:drying_recipe' });
     event.remove({ output: 'exdeorum:end_cake' });
-    event.remove({ output: 'thermal:dynamo_magmatic' });
     event.remove({ output: 'industrialforegoing:mycelial_magma' });
     event.remove({ type: 'exdeorum:compressed_sieve' });
     event.remove({ mod: 'simplyjetpacks' });
