4209523933614eb1b6f207e598cac59bcb9fd1d6
Jeremy <pawjwp@users.noreply.github.com>
2025-05-02 15:47:59 -0400

    Misc KubeJS cleanups

--- kubejs/server_scripts/flint_inventory.js
+++ kubejs/server_scripts/flint_inventory.js
@@ -1,8 +1,8 @@
 BlockEvents.rightClicked((event) => {
-  if (event.item.is("flint")) {
-    JsonIO.write("kubejs/inventory.json", {
-      items: event.block.inventory.allItems.map((item) => item.id),
-    });
-    event.cancel();
-  }
+    if (event.item.is("flint")) {
+        JsonIO.write("kubejs/inventory.json", {
+            items: event.block.inventory.allItems.map((item) => item.id),
+        });
+        event.cancel();
+    }
 });
--- kubejs/server_scripts/tags/slime_blood.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.tags('item', event => {
-  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
-  event.add('forge:slimeball/blood', 'kube_js:coagulated_blood')
-  event.add('exdeorum:hammers', 'tconstruct:vein_hammer')
-  event.add('exdeorum:compressed_hammers', 'tconstruct:sledge_hammer')
-})
-
-
