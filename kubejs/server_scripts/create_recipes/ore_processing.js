df37f56ac8afa6df73e82beff9d4f859cdf6f56e
Jeremy <pawjwp@users.noreply.github.com>
2025-05-06 20:03:19 -0400

    Fixes to merged machine frames and ore proccessing
    Moved machine frame unification to AlmostUnified config from KJS script
    Updated remaining recipes that still used steel casings instead of machine frames
    Fixed machine frame recipe error
    Fixed ore processing recipe errors

--- kubejs/server_scripts/create_recipes/ore_processing.js
+++ kubejs/server_scripts/create_recipes/ore_processing.js
@@ -70,17 +70,17 @@ ServerEvents.recipes(event => {
 	};
 
 	ore_processing('#forge:ores/iron', 'create:crushed_raw_iron', 'thermal:iron_dust', '#forge:ingots/iron', 250, 1, 0.25, 1, 0.75);
-	ore_processing('#forge:ores/copper', 'create:crushed_raw_copper', 'thermal:copper_dust', '#forge:ingots/copper', 250, 1, 0.25, 1, 0.75);
-	ore_processing('#forge:ores/gold', 'create:crushed_raw_gold', 'thermal:gold_dust', '#forge:ingots/gold', 250, 3, 0.75, 5, 0.25);
+	ore_processing('#forge:ores/copper', 'create:crushed_raw_copper', 'thermal:copper_dust', '#forge:ingots/copper', 250, 3, 0.75, 5, 0.25);
+	ore_processing('#forge:ores/gold', 'create:crushed_raw_gold', 'thermal:gold_dust', '#forge:ingots/gold', 250, 1, 0.25, 1, 0.75);
 	ore_processing('#forge:ores/zinc', 'create:crushed_raw_zinc', '', '#forge:ingots/zinc', 250, 1, 0.25, 1, 0.75);
-	ore_processing('#forge:ores/osmium', 'create:crushed_raw_osmium', 'mekanism:osmium_dust', '#forge:ingots/osmium', 250, 1, 0.25, 1, 0.75);
+	ore_processing('#forge:ores/osmium', 'create:crushed_raw_osmium', 'mekanism:dust_osmium', '#forge:ingots/osmium', 250, 1, 0.25, 1, 0.75);
 	ore_processing('#forge:ores/silver', 'create:crushed_raw_silver', 'thermal:silver_dust', '#forge:ingots/silver', 250, 1, 0.25, 1, 0.75);
 	ore_processing('#forge:ores/tin', 'create:crushed_raw_tin', 'thermal:tin_dust', '#forge:ingots/tin', 250, 1, 0.25, 1, 0.75);
 	ore_processing('#forge:ores/lead', 'create:crushed_raw_lead', 'thermal:lead_dust', '#forge:ingots/lead', 250, 1, 0.25, 1, 0.75);
-	ore_processing('#forge:ores/uranium', 'create:crushed_raw_uranium', 'mekanism:uranium_dust', '#forge:ingots/uranium', 250, 1, 0.25, 1, 0.75);
+	ore_processing('#forge:ores/uranium', 'create:crushed_raw_uranium', 'mekanism:dust_uranium', '#forge:ingots/uranium', 250, 1, 0.25, 1, 0.75);
 	ore_processing('#forge:ores/nickel', 'create:crushed_raw_nickel', 'thermal:nickel_dust', '#forge:ingots/nickel', 250, 1, 0.25, 1, 0.75);
 
-	ore_processing('', '', 'mekanism:refined_obsidian_dust', '#forge:ingots/refined_obsidian', 250, 1, 0.25, 1, 0.75);
+	ore_processing('', '', 'mekanism:dust_refined_obsidian', '#forge:ingots/refined_obsidian', 250, 1, 0.25, 1, 0.75);
 	ore_processing('', '', 'thermal:netherite_dust', '#forge:ingots/netherite', 250, 1, 0.25, 1, 0.75);
 	ore_processing('', '', 'thermal:steel_dust', '#forge:ingots/steel', 250, 1, 0.25, 1, 0.75);
 	ore_processing('', '', 'thermal:rose_gold_dust', '#forge:ingots/rose_gold', 250, 1, 0.25, 1, 0.75);
--- kubejs/server_scripts/machine_frame_replacement.js
+++ kubejs/server_scripts/machine_frame_replacement.js
@@ -1,4 +1,15 @@
 ServerEvents.recipes(event => {
+// This section now handled in config/almostunified/unify.json
+/*
+  event.replaceInput({ input: 'industrialforegoing:machine_frame_pity' }, 'industrialforegoing:machine_frame_pity', 'crash_landing:machine_frames');
+  event.replaceInput({ input: '#industrialforegoing:machine_frame/pity' }, '#industrialforegoing:machine_frame/pity', 'crash_landing:machine_frames');
+  event.replaceInput({ input: 'industrialforegoing:machine_frame_simple' }, 'industrialforegoing:machine_frame_simple', 'crash_landing:machine_frames');
+  event.replaceInput({ input: '#industrialforegoing:machine_frame/simple' }, '#industrialforegoing:machine_frame/simple', 'crash_landing:machine_frames');
+  event.replaceInput({ input: 'mekanism:steel_casing' }, 'mekanism:steel_casing', 'crash_landing:machine_frames');
+  event.replaceInput({ input: 'enderio:void_chassis' }, 'enderio:void_chassis', 'crash_landing:machine_frames');
+  event.replaceInput({ input: 'enderio:ensouled_chassis' }, 'enderio:ensouled_chassis', '#crash_landing:machine_frames');
+  event.replaceInput({ input: 'rftoolsbase:machine_frame' }, 'rftoolsbase:machine_frame', '#crash_landing:machine_frames');
+*/
   event.remove({ output: 'thermal:machine_frame' });
   event.remove({ output: 'industrialforegoing:machine_frame_pity' });
   event.remove({ output: 'industrialforegoing:machine_frame_simple' });
@@ -6,15 +17,6 @@ ServerEvents.recipes(event => {
   event.remove({ output: 'enderio:void_chassis' });
   event.remove({ output: 'enderio:ensouled_chassis' });
   event.remove({ output: 'rftoolsbase:machine_frame' });
-
-  event.replaceInput({ input: 'industrialforegoing:machine_frame_pity' }, 'industrialforegoing:machine_frame_pity', 'thermal:machine_frame');
-  event.replaceInput({ input: '#industrialforegoing:machine_frame/pity' }, '#industrialforegoing:machine_frame/pity', 'thermal:machine_frame');
-  event.replaceInput({ input: 'industrialforegoing:machine_frame_simple' }, 'industrialforegoing:machine_frame_simple', 'thermal:machine_frame');
-  event.replaceInput({ input: '#industrialforegoing:machine_frame/simple' }, '#industrialforegoing:machine_frame/simple', 'thermal:machine_frame');
-  event.replaceInput({ input: 'mekanism:steel_casing' }, 'mekanism:steel_casing', 'thermal:machine_frame');
-  event.replaceInput({ input: 'enderio:void_chassis' }, 'enderio:void_chassis', 'thermal:machine_frame');
-  event.replaceInput({ input: 'enderio:ensouled_chassis' }, 'enderio:ensouled_chassis', 'thermal:ensouled_chassis');
-  event.replaceInput({ input: 'rftoolsbase:machine_frame' }, 'rftoolsbase:machine_frame', 'thermal:machine_frame');
 
   event.shaped(
     Item.of('thermal:machine_frame', 1),
@@ -24,9 +26,9 @@ ServerEvents.recipes(event => {
       'SGS'
     ],
     {
-      E: '#forge:ingots/steel',
       G: '#forge:glass',
-      P: 'pneumaticcraft:printed_circuit_board'
+      P: 'pneumaticcraft:printed_circuit_board',
+      S: '#forge:ingots/steel'
     }
   )
 })
