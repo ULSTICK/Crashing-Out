53eafd702cc3401913f0d5f5439c34fd2e261934
Jeremy <pawjwp@users.noreply.github.com>
2025-05-08 17:12:29 -0400

    Fixed machine frame bug and other tweaks
    Fixed a bug with certain Industrial Foregoing machines not using universal machine frames for their recipes
    Hid AE2 facades in JEI
    Quest book now stays in inventory upon death
    Added endstone to stone types in unification config

--- kubejs/server_scripts/replacements/machine_frame_replacement.js
+++ kubejs/server_scripts/replacements/machine_frame_replacement.js
@@ -1,14 +1,14 @@
 ServerEvents.recipes(event => {
-// This section now handled in config/almostunified/unify.json
+// This section now partially handled in config/almostunified/unify.json
 /*
-  event.replaceInput({ input: 'industrialforegoing:machine_frame_pity' }, 'industrialforegoing:machine_frame_pity', 'crash_landing:machine_frames');
-  event.replaceInput({ input: '#industrialforegoing:machine_frame/pity' }, '#industrialforegoing:machine_frame/pity', 'crash_landing:machine_frames');
-  event.replaceInput({ input: 'industrialforegoing:machine_frame_simple' }, 'industrialforegoing:machine_frame_simple', 'crash_landing:machine_frames');
-  event.replaceInput({ input: '#industrialforegoing:machine_frame/simple' }, '#industrialforegoing:machine_frame/simple', 'crash_landing:machine_frames');
-  event.replaceInput({ input: 'mekanism:steel_casing' }, 'mekanism:steel_casing', 'crash_landing:machine_frames');
-  event.replaceInput({ input: 'enderio:void_chassis' }, 'enderio:void_chassis', 'crash_landing:machine_frames');
-  event.replaceInput({ input: 'enderio:ensouled_chassis' }, 'enderio:ensouled_chassis', '#crash_landing:machine_frames');
-  event.replaceInput({ input: 'rftoolsbase:machine_frame' }, 'rftoolsbase:machine_frame', '#crash_landing:machine_frames');
+  event.replaceInput({ input: 'industrialforegoing:machine_frame_pity' }, 'industrialforegoing:machine_frame_pity', 'crash_landing:machine_frame');
+  event.replaceInput({ input: '#industrialforegoing:machine_frame/pity' }, '#industrialforegoing:machine_frame/pity', 'crash_landing:machine_frame');
+  event.replaceInput({ input: 'industrialforegoing:machine_frame_simple' }, 'industrialforegoing:machine_frame_simple', 'crash_landing:machine_frame');
+  event.replaceInput({ input: '#industrialforegoing:machine_frame/simple' }, '#industrialforegoing:machine_frame/simple', 'crash_landing:machine_frame');
+  event.replaceInput({ input: 'mekanism:steel_casing' }, 'mekanism:steel_casing', 'crash_landing:machine_frame');
+  event.replaceInput({ input: 'enderio:void_chassis' }, 'enderio:void_chassis', 'crash_landing:machine_frame');
+  event.replaceInput({ input: 'enderio:ensouled_chassis' }, 'enderio:ensouled_chassis', '#crash_landing:machine_frame');
+  event.replaceInput({ input: 'rftoolsbase:machine_frame' }, 'rftoolsbase:machine_frame', '#crash_landing:machine_frame');
 */
   event.remove({ output: 'thermal:machine_frame' });
   event.remove({ output: 'industrialforegoing:machine_frame_pity' });
