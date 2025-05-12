9a3739a710d2504e956d5a0ef383d72e8096369f
Jeremy <pawjwp@users.noreply.github.com>
2025-05-12 19:39:30 -0400

    Steel recipe fix, crash report mod, other fixes
    Fixed missing Steel recipes by stopping AlmostUnified from deduplicating Ad Astra recipes
    Changed Ad Astra recipes for the rover and its components
    Added Crash Assistant Mod
    Fully removed disabled mods to prevent some modloaders from accidentally enabling them

--- kubejs/server_scripts/ad_astra/ad_astra_removal.js
+++ kubejs/server_scripts/ad_astra/ad_astra_removal.js
@@ -1,6 +1,8 @@
 ServerEvents.recipes(event => {
+    // Removes all Ad Astra recipes. When this line is removed, remove the "ad_astra:*+" line from AlmostUnified's duplicates.json
     event.remove({ mod: 'ad_astra' });
 
+    event.remove({ output: 'ad_astra:wheel' });
     event.shaped(
         Item.of('ad_astra:wheel', 1),
         [
@@ -14,18 +16,36 @@ ServerEvents.recipes(event => {
         }
     )
 
+    event.remove({ output: 'ad_astra:radio' });
+    event.shaped(
+        Item.of('ad_astra:radio', 1),
+        [
+            'W  ',
+            'SPS',
+            'SSS'
+        ],
+        {
+            P: 'pneumaticcraft:printed_circuit_board',
+            S: '#forge:plates/steel',
+            W: '#forge:wires/all_metal'
+        }
+    )
+
+    event.remove({ output: 'ad_astra:tier_1_rover' });
     event.shaped(
         Item.of('ad_astra:tier_1_rover', 1),
         [
-            'P P',
-            'CDF',
-            'WPW'
+            'B P',
+            'GRE',
+            'WSW'
         ],
         {
-            C: '#forge:chests',
-            D: 'thermal:dynamo_compression',
-            F: 'mekanism:basic_fluid_tank',
-            P: '#forge:plates/steel',
+            B: ['#forge:storage_blocks/desh', '#forge:storage_blocks/bronze'],
+            E: ['ad_astra:desh_engine', 'thermal:dynamo_compression'],
+            G: ['ad_astra:large_gas_tank', 'mekanism:basic_fluid_tank'],
+            P: 'pneumaticcraft:printed_circuit_board',
+            R: 'ad_astra:radio',
+            S: ['#forge:plates/desh', '#forge:plates/bronze'],
             W: 'ad_astra:wheel'
         }
     )
