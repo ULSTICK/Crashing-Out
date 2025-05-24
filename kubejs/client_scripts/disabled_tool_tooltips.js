38891043b85435f0874720528bdf68d0270adb4a
Jeremy <pawjwp@users.noreply.github.com>
2025-05-23 23:40:10 -0400

    Rebalanced plastic and plastic scrap costs, tweaks
    Plastic:
    - Unified liquid plastic to plastic sheet conversion rate (1 bucket because the in-world solidification is not configurable)
    - Split plastic scrap melting into different recipes based on the amount of plastic in the source item
    - Increased plastic scrap spawn count in cities
    - Changed HDPE sheet recipe to cost 4 HDPE pellets (from 3)
    - Changed HDPE rod recipe to cost 2 HDPE pellets (from 4)
    Changed disabled tool tooltips to red color and placed them under the item name
    Disabled mini hud by default

--- kubejs/client_scripts/disabled_tool_tooltips.js
+++ kubejs/client_scripts/disabled_tool_tooltips.js
@@ -1,62 +1,66 @@
-const toolsmod = [
-    "minecraft:wooden_sword",
-    "minecraft:wooden_pickaxe",
-    "minecraft:wooden_shovel",
-    "minecraft:wooden_axe",
-    "minecraft:wooden_hoe",
-
-    "minecraft:stone_sword",
-    "minecraft:stone_pickaxe",
-    "minecraft:stone_shovel",
-    "minecraft:stone_axe",
-    "minecraft:stone_hoe",
-
-    "minecraft:iron_sword",
-    "minecraft:iron_pickaxe",
-    "minecraft:iron_shovel",
-    "minecraft:iron_axe",
-    "minecraft:iron_hoe",
-
-    "minecraft:golden_sword",
-    "minecraft:golden_pickaxe",
-    "minecraft:golden_shovel",
-    "minecraft:golden_axe",
-    "minecraft:golden_hoe",
-
-    "minecraft:diamond_sword",
-    "minecraft:diamond_pickaxe",
-    "minecraft:diamond_shovel",
-    "minecraft:diamond_axe",
-    "minecraft:diamond_hoe",
-
-    "minecraft:netherite_sword",
-    "minecraft:netherite_pickaxe",
-    "minecraft:netherite_shovel",
-    "minecraft:netherite_axe",
-    "minecraft:netherite_hoe",
-
-    "minecraft:trident",
-    "minecraft:shield",
-
-    "ae2:nether_quartz_sword",
-    "ae2:nether_quartz_pickaxe",
-    "ae2:nether_quartz_shovel",
-    "ae2:nether_quartz_axe",
-    "ae2:nether_quartz_hoe",
-
-    "ae2:certus_quartz_sword",
-    "ae2:certus_quartz_pickaxe",
-    "ae2:certus_quartz_shovel",
-    "ae2:certus_quartz_axe",
-    "ae2:certus_quartz_hoe",
-
-    "ae2:fluix_sword",
-    "ae2:fluix_pickaxe",
-    "ae2:fluix_shovel",
-    "ae2:fluix_axe",
-    "ae2:fluix_hoe"
-];
-
 ItemEvents.tooltip(event => {
-    toolsmod.forEach(tool => event.add(tool, "This item is non-functional. Please use Tinkers Construct tools instead."));
+    const toolsmod = [
+        "minecraft:wooden_sword",
+        "minecraft:wooden_pickaxe",
+        "minecraft:wooden_shovel",
+        "minecraft:wooden_axe",
+        "minecraft:wooden_hoe",
+    
+        "minecraft:stone_sword",
+        "minecraft:stone_pickaxe",
+        "minecraft:stone_shovel",
+        "minecraft:stone_axe",
+        "minecraft:stone_hoe",
+    
+        "minecraft:iron_sword",
+        "minecraft:iron_pickaxe",
+        "minecraft:iron_shovel",
+        "minecraft:iron_axe",
+        "minecraft:iron_hoe",
+    
+        "minecraft:golden_sword",
+        "minecraft:golden_pickaxe",
+        "minecraft:golden_shovel",
+        "minecraft:golden_axe",
+        "minecraft:golden_hoe",
+    
+        "minecraft:diamond_sword",
+        "minecraft:diamond_pickaxe",
+        "minecraft:diamond_shovel",
+        "minecraft:diamond_axe",
+        "minecraft:diamond_hoe",
+    
+        "minecraft:netherite_sword",
+        "minecraft:netherite_pickaxe",
+        "minecraft:netherite_shovel",
+        "minecraft:netherite_axe",
+        "minecraft:netherite_hoe",
+    
+        "minecraft:trident",
+        "minecraft:shield",
+    
+        "ae2:nether_quartz_sword",
+        "ae2:nether_quartz_pickaxe",
+        "ae2:nether_quartz_shovel",
+        "ae2:nether_quartz_axe",
+        "ae2:nether_quartz_hoe",
+    
+        "ae2:certus_quartz_sword",
+        "ae2:certus_quartz_pickaxe",
+        "ae2:certus_quartz_shovel",
+        "ae2:certus_quartz_axe",
+        "ae2:certus_quartz_hoe",
+    
+        "ae2:fluix_sword",
+        "ae2:fluix_pickaxe",
+        "ae2:fluix_shovel",
+        "ae2:fluix_axe",
+        "ae2:fluix_hoe"
+    ];
+
+    toolsmod.forEach(tool => {
+        event.addAdvanced(tool, (item, advanced, text) => {
+            text.add(1, Text.darkRed("This item is non-functional. Please use Tinkers Construct tools instead."));
+        });
+    });
 });
