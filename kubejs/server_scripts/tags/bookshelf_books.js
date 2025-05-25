f3e68ceeab43d95141cca0ceb3cb6e1a7caf238a
Jeremy <pawjwp@users.noreply.github.com>
2025-05-25 10:53:34 -0400

    Crumbling seared bricks to seared brick conversion
    Fixed crumbling seared bricks not being breakable to turn into seared bricks
    Changed methods to use kubejs instead of a datapack
    Added methods to turn crumbling bricks into seared bricks with the pulverizer, crusher, and miller
    Increased max JEI height and width
    Added some mods' guidebooks to the bookshelf_books tag

---
+++ kubejs/server_scripts/tags/bookshelf_books.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('item', event => {
+    event.add('minecraft:bookshelf_books', 'ae2:guide')
+    event.add('minecraft:bookshelf_books', 'cookingforblockheads:recipe_book')
+    event.add('minecraft:bookshelf_books', 'cookingforblockheads:crafting_book')
+    event.add('minecraft:bookshelf_books', 'hardcorequesting:quest_book')
+    event.add('minecraft:bookshelf_books', 'hardcorequesting:enabled_quest_book')
+    event.add('minecraft:bookshelf_books', 'rftools:manual')
+})
