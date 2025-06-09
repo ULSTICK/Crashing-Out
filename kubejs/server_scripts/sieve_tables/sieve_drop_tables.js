5f69c37b4842882c81f744216c58609875d9b4ca
Jeremy <pawjwp@users.noreply.github.com>
2025-06-09 17:21:05 -0400

    Added Dirt and End Stone to reworked sieve script
    Updated the reworked sieve script to include drops for dirt and end stone
    The only remaining sieve results left to modify are podzol and moss, but those are a lower priority and more complex

--- kubejs/server_scripts/sieve_tables/sieve_drop_tables.js
+++ kubejs/server_scripts/sieve_tables/sieve_drop_tables.js
@@ -188,6 +188,36 @@ ServerEvents.recipes(event => {
     );
 
     registerSieveTable(
+        'minecraft:dirt',
+        'exdeorum:compressed_dirt',
+        `
+            exdeorum:stone_pebble	180	216	252	288	324	360
+            exdeorum:grass_seeds	8	9.6	11.2	12.8	14.4	16
+            pamhc2crops:riceseeditem	6	7.2	8.4	9.6	10.8	12
+            minecraft:sugar_cane	4	4.8	5.6	6.4	7.2	8
+            minecraft:oak_sapling	3	3.6	4.2	4.8	5.4	6
+            thermal:rubberwood_sapling	2.5	3	3.5	4	4.5	5
+            pamhc2crops:soybeanseeditem	2.5	3	3.5	4	4.5	5
+            pamhc2crops:peanutseeditem	2	2.4	2.8	3.2	3.6	4
+            pamhc2crops:tomatoseeditem	2	2.4	2.8	3.2	3.6	4
+            pamhc2crops:grapeseeditem	1.5	1.8	2.1	2.4	2.7	3
+            pamhc2foodcore:sunflowerseedsitem	1.5	1.8	2.1	2.4	2.7	3
+            minecraft:birch_sapling	1	1.2	1.4	1.6	1.8	2
+            minecraft:spruce_sapling	1	1.2	1.4	1.6	1.8	2
+            minecraft:sweet_berries	1	1.2	1.4	1.6	1.8	2
+            pamhc2crops:spiceleafseeditem	1	1.2	1.4	1.6	1.8	2
+            pamhc2crops:oatsseeditem	1	1.2	1.4	1.6	1.8	2
+            minecraft:melon_seeds	1	1.2	1.4	1.6	1.8	2
+            minecraft:bamboo	1	1.2	1.4	1.6	1.8	2
+            minecraft:cactus	1	1.2	1.4	1.6	1.8	2
+            exdeorum:mycelium_spores	0.3	1	1.2	1.4	1.6	1.8
+            minecraft:dark_oak_sapling	0.5	0.6	0.7	0.8	0.9	1
+            minecraft:jungle_sapling	0.3	0.3	0.4	0.4	0.5	0.5
+            minecraft:acacia_sapling	0.3	0.3	0.4	0.4	0.5	0.5
+        `
+    );
+
+    registerSieveTable(
         'exdeorum:crushed_netherrack',
         'exdeorum:compressed_crushed_netherrack',
         `
@@ -242,6 +272,20 @@ ServerEvents.recipes(event => {
             minecraft:ghast_tear	0	0	0.8	3	3.6	4.2
             minecraft:magma_cream	2.5	3	3.5	4	4.5	5
             minecraft:netherite_scrap	0	0	0	0.1	0.5	0.6
+        `
+    );
+
+    registerSieveTable(
+        'exdeorum:crushed_end_stone',
+        'exdeorum:compressed_crushed_end_stone',
+        `
+            minecraft:chorus_fruit	4	4.8	5.6	6.4	7.2	8
+            minecraft:ender_pearl	0.8	3	3.6	4.2	4.8	5.4
+            tconstruct:ender_slime_sapling	2	2.4	2.8	3.2	3.6	4
+            tconstruct:ender_slime_ball	2	2.4	2.8	3.2	3.6	4
+            tconstruct:ender_slime_grass_seeds	1	1.2	1.4	1.6	1.8	2
+            minecraft:ender_eye	0	0.3	1	1.2	1.4	1.6
+            minecraft:chorus_flower	0.3	1	1.2	1.4	1.6	1.8
         `
     );
 });
