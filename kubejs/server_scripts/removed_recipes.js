afe038755e621a9adb15274b9d3502f4bcbdec66
Jeremy <pawjwp@users.noreply.github.com>
2025-07-01 16:06:04 -0400

    More recipe removal tweaks
    Removed duplicate gear recipes, Thermal recipe will be the standard
    Removed netherite boots as they were the only netherite armor piece that was allowed for some reason
    Changed formatting slightly

--- kubejs/server_scripts/removed_recipes.js
+++ kubejs/server_scripts/removed_recipes.js
@@ -2,80 +2,85 @@ ServerEvents.recipes(event => {
     // Removal by ID
     event.remove({
         id:
-        [
-            "tconstruct:molten_pewter",
-            "tconstruct:smeltery/casting/scorched/brick_composite",
-            "tconstruct:smeltery/casting/scorched/stone_from_magma",
-            "tconstruct:smeltery/casting/seared/brick_composite",
-            "tconstruct:smeltery/casting/seared/stone/block_from_seared",
-            "tconstruct:smeltery/melting/metal/iron/ore_sparse"
-        ]
+            [
+                "enderio:iron_gear",
+                "industrialforegoing:diamond_gear",
+                "industrialforegoing:gold_gear",
+                "industrialforegoing:iron_gear",
+                "tconstruct:molten_pewter",
+                "tconstruct:smeltery/casting/scorched/brick_composite",
+                "tconstruct:smeltery/casting/scorched/stone_from_magma",
+                "tconstruct:smeltery/casting/seared/brick_composite",
+                "tconstruct:smeltery/casting/seared/stone/block_from_seared",
+                "tconstruct:smeltery/melting/metal/iron/ore_sparse"
+            ]
     });
 
     // Removal by mod
     event.remove({
         mod:
-        [
-            "cgm",
-            "mob_grinding_utils",
-            "simplyjetpacks"
-        ]
+            [
+                "cgm",
+                "mob_grinding_utils",
+                "simplyjetpacks"
+            ]
     });
 
     // Removal by output
     event.remove({
         output:
-        [
-            "cookingforblockheads:sink",
-            "create:netherite_diving_boots",
-            "exdeorum:acacia_crucible",
-            "exdeorum:bamboo_crucible",
-            "exdeorum:birch_crucible",
-            "exdeorum:crimson_crucible",
-            "exdeorum:dark_oak_crucible",
-            "exdeorum:end_cake",
-            "exdeorum:jungle_crucible",
-            "exdeorum:oak_crucible",
-            "exdeorum:porcelain_bucket",
-            "exdeorum:spruce_crucible",
-            "exdeorum:warped_crucible",
-            "industrialforegoing:mycelial_magma",
-            "mekanismgenerators:heat_generator",
-            "minecraft:composter",
-            "minecraft:diamond_boots",
-            "minecraft:diamond_chestplate",
-            "minecraft:diamond_helmet",
-            "minecraft:diamond_leggings",
-            "minecraft:netherite_chestplate",
-            "minecraft:netherite_helmet",
-            "minecraft:netherite_leggings",
-            "survive:charcoal_filter",
-            "survive:charcoal_filter",
-            "tconstruct:grout",
-            "tconstruct:nether_grout",
-            "torchmaster:megatorch"
-        ]
+            [
+                "cookingforblockheads:sink",
+                "create:netherite_diving_boots",
+                "exdeorum:acacia_crucible",
+                "exdeorum:bamboo_crucible",
+                "exdeorum:birch_crucible",
+                "exdeorum:crimson_crucible",
+                "exdeorum:dark_oak_crucible",
+                "exdeorum:end_cake",
+                "exdeorum:jungle_crucible",
+                "exdeorum:oak_crucible",
+                "exdeorum:porcelain_bucket",
+                "exdeorum:spruce_crucible",
+                "exdeorum:warped_crucible",
+                "industrialforegoing:mycelial_magma",
+                "mekanismgenerators:heat_generator",
+                "minecraft:composter",
+                "minecraft:diamond_boots",
+                "minecraft:diamond_chestplate",
+                "minecraft:diamond_helmet",
+                "minecraft:diamond_leggings",
+                "minecraft:netherite_boots",
+                "minecraft:netherite_chestplate",
+                "minecraft:netherite_helmet",
+                "minecraft:netherite_leggings",
+                "survive:charcoal_filter",
+                "survive:charcoal_filter",
+                "tconstruct:grout",
+                "tconstruct:nether_grout",
+                "torchmaster:megatorch"
+            ]
     });
 
     // Removal by type
     event.remove({
         type:
-        [
-            "create:potions",
-            "exdeorum:compressed_sieve",
-            "exdeorum:water_crucible",
-            "tconstruct:casting_table_potion"
-        ]
+            [
+                "create:potions",
+                "exdeorum:compressed_sieve",
+                "exdeorum:water_crucible",
+                "tconstruct:casting_table_potion"
+            ]
     });
 
     // Removal of haunting recipes
     event.remove({
         type: "create:haunting",
         output:
-        [
-            "minecraft:soul_sand",
-            "minecraft:soul_soil"
-        ]
+            [
+                "minecraft:soul_sand",
+                "minecraft:soul_soil"
+            ]
     });
 
     // Removal of fire charge recipes for Thermal alloys
@@ -84,21 +89,21 @@ ServerEvents.recipes(event => {
         type: "minecraft:crafting_shapeless",
         input: "minecraft:fire_charge",
         output:
-        [
-            "thermal:bronze_ingot",
-            "thermal:constantan_ingot",
-            "thermal:electrum_ingot",
-            "thermal:enderium_ingot",
-            "thermal:invar_ingot",
-            "thermal:lumium_ingot",
-            "thermal:signalum_ingot"
-        ]
+            [
+                "thermal:bronze_ingot",
+                "thermal:constantan_ingot",
+                "thermal:electrum_ingot",
+                "thermal:enderium_ingot",
+                "thermal:invar_ingot",
+                "thermal:lumium_ingot",
+                "thermal:signalum_ingot"
+            ]
     });
 
     // Misc removals
     event.remove({ type: "minecraft:crafting_shaped", output: "minecraft:coarse_dirt" });
     event.remove({ type: "tconstruct:casting_table", output: "mekanism:ingot_refined_obsidian" });
     event.remove({ type: "tconstruct:casting_table", output: "minecraft:potion" });
-    
+
     exdeorum.removeDefaultSieveRecipes(event);
 });
