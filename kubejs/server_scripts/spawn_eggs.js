1a2c058a8727a317ae8e8250e5b90df4b9744065
Jeremy <pawjwp@users.noreply.github.com>
2025-07-02 14:52:26 -0400

    Added Spawn Egg crafting recipes
    Added spawn egg crafting recipes using the dissolution chamber, to replace the spawn egg quests
    Added KubeJS Industrial Foregoing addon to support this process
    Changed the tank sizes of Industrial Foregoing machines for consistency and to allow more liquids in dissolution chamber inputs

---
+++ kubejs/server_scripts/spawn_eggs.js
@@ -0,0 +1,308 @@
+ServerEvents.recipes(event => {
+
+    // cow spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:leather',
+            '#forge:leather',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:cow_spawn_egg',
+        300
+    )
+
+    // sheep spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:wool',
+            '#forge:wool',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:sheep_spawn_egg',
+        300
+    )
+
+    // pig spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:pig_spawn_egg',
+        300
+    )
+
+    // chicken spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds'
+        ],
+        Fluid.of('industrialforegoing:essence', 16000),
+        'minecraft:chicken_spawn_egg',
+        300
+    )
+
+    // horse spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/apple',
+            '#forge:crops/apple',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat'
+        ],
+        Fluid.of('industrialforegoing:essence', 64000),
+        'minecraft:horse_spawn_egg',
+        300
+    )
+
+    // donkey spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:chests',
+            '#forge:chests',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat',
+            '#forge:crops/wheat'
+        ],
+        Fluid.of('industrialforegoing:essence', 64000),
+        'minecraft:donkey_spawn_egg',
+        300
+    )
+
+    // rabbit spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            'minecraft:dandelion',
+            'minecraft:dandelion',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot',
+            '#forge:crops/carrot'
+        ],
+        Fluid.of('industrialforegoing:essence', 16000),
+        'minecraft:rabbit_spawn_egg',
+        300
+    )
+
+    // wolf spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:bones',
+            '#forge:bones',
+            '#forge:bones',
+            '#forge:bones',
+            '#forge:bones',
+            '#forge:bones',
+            '#forge:bones',
+            '#forge:bones'
+        ],
+        Fluid.of('industrialforegoing:essence', 64000),
+        'minecraft:wolf_spawn_egg',
+        300
+    )
+
+    // ocelot spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:fishes',
+            '#forge:fishes',
+            '#forge:fishes',
+            'minecraft:jungle_sapling',
+            'minecraft:jungle_sapling',
+            '#forge:fishes',
+            '#forge:fishes',
+            '#forge:fishes'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:ocelot_spawn_egg',
+        300
+    )
+
+    // fox spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:berries/sweetberry',
+            '#forge:berries/sweetberry',
+            '#forge:berries/sweetberry',
+            'minecraft:spruce_sapling',
+            'minecraft:spruce_sapling',
+            '#forge:berries/sweetberry',
+            '#forge:berries/sweetberry',
+            '#forge:berries/sweetberry'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:fox_spawn_egg',
+        300
+    )
+
+    // parrot spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds',
+            'minecraft:jungle_sapling',
+            'minecraft:jungle_sapling',
+            '#forge:seeds',
+            '#forge:seeds',
+            '#forge:seeds'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:parrot_spawn_egg',
+        300
+    )
+
+    // bee spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#minecraft:flowers',
+            '#minecraft:flowers',
+            '#minecraft:flowers',
+            '#minecraft:flowers',
+            '#minecraft:flowers',
+            '#minecraft:flowers',
+            '#minecraft:flowers',
+            '#minecraft:flowers'
+        ],
+        Fluid.of('industrialforegoing:essence', 16000),
+        'minecraft:bee_spawn_egg',
+        300
+    )
+
+    // frog spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:slimeballs',
+            '#forge:slimeballs',
+            '#forge:slimeballs',
+            '#forge:slimeballs',
+            '#forge:slimeballs',
+            '#forge:slimeballs',
+            '#forge:slimeballs',
+            '#forge:slimeballs'
+        ],
+        Fluid.of('industrialforegoing:essence', 16000),
+        'minecraft:frog_spawn_egg',
+        300
+    )
+
+    // turtle spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            'minecraft:seagrass',
+            'minecraft:seagrass',
+            'minecraft:seagrass',
+            'minecraft:water_bucket',
+            'minecraft:water_bucket',
+            'minecraft:seagrass',
+            'minecraft:seagrass',
+            'minecraft:seagrass'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:turtle_spawn_egg',
+        300
+    )
+
+    // squid spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:dyes/black',
+            '#forge:dyes/black',
+            '#forge:dyes/black',
+            'minecraft:water_bucket',
+            'minecraft:water_bucket',
+            '#forge:dyes/black',
+            '#forge:dyes/black',
+            '#forge:dyes/black'
+        ],
+        Fluid.of('industrialforegoing:essence', 16000),
+        'minecraft:squid_spawn_egg',
+        300
+    )
+
+    // glow squid spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:dusts/glowstone',
+            '#forge:dusts/glowstone',
+            '#forge:dusts/glowstone',
+            'minecraft:water_bucket',
+            'minecraft:water_bucket',
+            '#forge:dyes/black',
+            '#forge:dyes/black',
+            '#forge:dyes/black'
+        ],
+        Fluid.of('industrialforegoing:essence', 16000),
+        'minecraft:glow_squid_spawn_egg',
+        300
+    )
+
+    // dolphin spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:fishes',
+            '#forge:fishes',
+            '#forge:fishes',
+            'minecraft:water_bucket',
+            'minecraft:water_bucket',
+            '#forge:dusts/prismarine',
+            '#forge:dusts/prismarine',
+            '#forge:dusts/prismarine'
+        ],
+        Fluid.of('industrialforegoing:essence', 64000),
+        'minecraft:dolphin_spawn_egg',
+        300
+    )
+
+    // axolotl spawn egg recipe
+    event.recipes.industrialforegoing.dissolution_chamber(
+        [
+            '#forge:fishes/rawtropicalfish',
+            '#forge:fishes/rawtropicalfish',
+            '#forge:fishes/rawtropicalfish',
+            'minecraft:water_bucket',
+            'minecraft:water_bucket',
+            '#forge:stone',
+            '#forge:stone',
+            '#forge:stone'
+        ],
+        Fluid.of('industrialforegoing:essence', 32000),
+        'minecraft:axolotl_spawn_egg',
+        300
+    )
+});
