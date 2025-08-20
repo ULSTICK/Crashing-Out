3b1986cb073689a4258038e057cf2270a0729d68
Jeremy <pawjwp@users.noreply.github.com>
2025-08-20 00:46:07 -0400

    Fixed reactor controller issue
    Fixed reactor spawning the wrong type of reactor controller
    Tweaked tags and quest to allow previously generated reactor terminals to complete the quest
    Tweaked line endings of PneumaticCraft configs to hopefully stop reuploads in the future

--- kubejs/server_scripts/tags/misc.js
+++ kubejs/server_scripts/tags/misc.js
@@ -2,4 +2,9 @@ ServerEvents.tags('item', event => {
     event.add('forge:storage_blocks/electrum',
         'createaddition:electrum_block'
     )
+
+    event.add('crash_landing:reactor_terminals',
+        'biggerreactors:reactor_terminal',
+        'kubejs:crashed_reactor_controller'
+    )
 });
